ccca54665c7e713b1ce25d1924df6839c4b0073e
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2023-06-26 19:47:18 +0700

    Update setmenu.sh

--- menu/setmenu.sh
+++ menu/setmenu.sh
@@ -10,44 +10,44 @@ echo -e "$y                   SYSTEM SETTING$wh"
 echo -e "$y-------------------------------------------------------------$wh"
 echo -e "$yy 1$y.  Ubah dan tambah domain/subdomain VPS"
 echo -e "$yy 2$y.  Ubah port VPS"
-echo -e "$yy 7$y.  Limit Bandwith Speed Server"
-echo -e "$yy 8$y.  Check Usage of VPS Ram"
-echo -e "$yy 9$y.  Reboot VPS"
-echo -e "$yy 10$y. Speedtest VPS"
-echo -e "$yy 11$y. Displaying System Information"
-echo -e "$yy 13$y. Menu"
-echo -e "$yy 14$y. Exit"
+echo -e "$yy 3$y.  Limit Bandwith Speed Server"
+echo -e "$yy 4$y.  Check Usage of VPS Ram"
+echo -e "$yy 5$y.  Reboot VPS"
+echo -e "$yy 6$y.  Speedtest VPS"
+echo -e "$yy 7$y.  Displaying System Information"
+echo -e "$yy 8$y.  Menu"
+echo -e "$yy 9$y.  Exit"
 echo -e "$y-------------------------------------------------------------$wh"
 echo -e ""
 read -p "Select From Options [ 1 - 60 ] : " menu
 echo -e ""
 case $menu in
 1)
-addhost
+host
 ;;
 2)
 changeport
 ;;
-7)
+3)
 limitspeed
 ;;
-8)
+4)
 ram
 ;;
-9)
+5)
 reboot
 ;;
-10)
+6)
 speedtest
 ;;
-11)
+7)
 info
 ;;
-13)
+8)
 clear
 menu
 ;;
-14)
+9)
 clear
 exit
 ;;
