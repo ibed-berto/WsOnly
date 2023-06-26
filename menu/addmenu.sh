91d0c3c44629fd2e2cb101edb9d9b1b08020139f
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2023-06-27 02:07:07 +0700

    Create addmenu.sh

---
+++ menu/addmenu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Download menu
+wget -O menu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/addssh.sh"
+wget -O autokill "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/autokill.sh"
+wget -O ceklimit "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/ceklimit.sh"
+wget -O cekssh "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/cekssh.sh"
+wget -O delexp "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/delexp.sh"
+wget -O delssh "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/delssh.sh"
+wget -O member "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/member.sh"
+wget -O restart "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/restart.sh"
+wget -O tendang "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/tendang.sh"
+
+wget -O info "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/info.sh"
+wget -O ipsaya "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ipsaya.sh"
+wget -O limitspeed "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/limitspeed.sh"
+wget -O menu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/menu.sh"
+
