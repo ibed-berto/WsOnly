3aa71e9928e62d129152b4ee3ad8685f566ec351
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2023-06-27 02:18:52 +0700

    Update addmenu.sh

--- menu/addmenu.sh
+++ menu/addmenu.sh
@@ -17,6 +17,16 @@ wget -O info "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/info
 wget -O ipsaya "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ipsaya.sh"
 wget -O limitspeed "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/limitspeed.sh"
 wget -O menu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/menu.sh"
+wget -O portsshnontls "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/portsshnontls.sh"
+wget -O portsshtls "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/portsshtls.sh"
+wget -O running "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/running.sh"
+wget -O setmenu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/setmenu.sh"
+wget -O sl-fix "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/sl-fix.sh"
+wget -O speedtest "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/speedtest.py"
+wget -O sshovpnmenu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/sshovpnmenu.sh"
+wget -O ubahport "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ubahport.sh"
+wget -O updatemenu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/updatemenu.sh"
+
 
 cd
 
