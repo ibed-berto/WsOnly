ed6d0a8f52574efd17d6372059d187521da4c8b1
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2023-06-26 12:03:56 +0700

    Create sl-fix.sh

---
+++ menu/sl-fix.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /usr/bin
+rm -rf restart
+rm -r restart
+rm restart
+rm -rf certssh
+rm -r certssh
+rm certssh
+rm -rf updatemenu
+rm -r updatemenu
+rm updatemenu
+
+cd /usr/bin
+wget -O certssh "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/ssl/cert.sh"
+wget -O restart "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/ssh/restart.sh"
+wget -O updatemenu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/updatemenu.sh"
+wget -O menu "https://raw.githubusercontent.com/ibed-berto/WsOnly/main/menu/menu.sh"
+
+
+cd /usr/bin
+chmod +x certssh
+chmod +x restart
+chmod +x updatemenu
+chmod +x menu
+
+cd /root/
