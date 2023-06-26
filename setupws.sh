e2a1bb14bb47c71ce81643eaa901f0f0264754a9
ibed-berto <70596636+ibed-berto@users.noreply.github.com>
2023-06-26 18:31:42 +0700

    Update setupws.sh

--- setupws.sh
+++ setupws.sh
@@ -9,6 +9,9 @@ if [ "$(systemd-detect-virt)" == "openvz" ]; then
 		exit 1
 fi
 
+#install host
+wget https://raw.githubusercontent.com/ibed-berto/WsOnly/main/ssl/host.sh && chmod +x host.sh && ./host.sh
+
 #install build
 wget https://raw.githubusercontent.com/ibed-berto/WsOnly/main/alat/build.sh && chmod +x build.sh && screen -S build ./build.sh
 
@@ -24,6 +27,7 @@ wget https://raw.githubusercontent.com/ibed-berto/WsOnly/main/stunnel5/stunnel5.
 #install ws
 wget https://raw.githubusercontent.com/ibed-berto/WsOnly/main/websocket/install.sh && chmod +x install.sh && screen -S install ./install.sh
 
+rm -f /root/host.sh
 rm -f /root/build.sh 
 rm -f /root/badvpn.sh
 rm -f /root/cert.sh
